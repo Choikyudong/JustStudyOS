4b9e8be31466f304327a9b5bdf83f0a8bead5263
Choikyudong <slgmgm@naver.com>
2026-02-04 22:48:56 +0900

    feat: lib 추가와 테스트 코드 추가
    - lib.rs는 cargo가 자동으로 인식하는 특별 파일이다.

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,23 +9,16 @@ use just_study_os::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello {}", "World!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     just_study_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
